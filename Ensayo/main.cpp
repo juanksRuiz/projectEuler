c51b0765d71832cc8b39b6d87322ea70f39fc513
juanksRuiz <juankruizo10@gmail.com>
2019-04-24 14:31:25 -0500

    Pinche core dumped

--- Ensayo/main.cpp
+++ Ensayo/main.cpp
@@ -5,21 +5,25 @@ using namespace std;
 
 bool isPrime(int n);
 vector<int> Primes(int n);
-int LargestPrimeFactor(int n);
+vector<int> LargestPrimeFactor(int n);
 
 int main(){
     int n = 11;
-    /*
     //cout << n << " es primo ? " << isPrime(n) << endl;
     vector<int> v = Primes(20);
     for(int i = 0; i < v.size(); i++){
-        cout << v[i] << ", " ;
+        cout << v[i] << endl;
     }
-    cout << endl;
-    */
-    int l = LargestPrimeFactor(n);
-    cout << "Divisor primo mas grande de " << n << endl;
-    cout << l <<  endl;
+
+    vector<int> div = LargestPrimeFactor(n);
+
+    int x = div.at(div.size());
+    cout << "divisor mas grande primo: " << x << endl;
+
+    //int l = LargestPrimeFactor(n);
+    //cout << "Divisor primo mas grande de " << endl;
+    //cout << l <<  endl;
+
 }
 
 bool isPrime(int n){
@@ -36,7 +40,7 @@ bool isPrime(int n){
         }
         return true;
     }
-    
+
 }
 vector<int> Primes(int n){
     vector<int> v;
@@ -49,7 +53,7 @@ vector<int> Primes(int n){
     return v;
 }
 
-int LargestPrimeFactor(int n){
+vector<int> LargestPrimeFactor(int n){
     //Retorna el divisor primo mas grande de n
     vector<int> v = Primes(n);
     vector<int> div;
@@ -58,9 +62,9 @@ int LargestPrimeFactor(int n){
             cout << v[i] << endl;
             div.push_back(v[i]);
         }
-        
+
     }
 
-    return div[div.size() - 1];
+    return div;
 
 }
